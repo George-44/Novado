1ee64c07379ab5c3d3aec5cad5ed2eb985c43377
George-44 <pridonashviligio04@gmail.com>
2024-04-26 22:37:15 +0400

    tasks

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-let = document.getElementById("btn");
+let x = document.getElementById('btn');
+
+x.onclick = addTask;
 
 function addTask() {
-    tasks-text-container.innerHTML == "<p>40000<p>";
-}
+    const taskbox = document.querySelector('.taskbox');
+    taskbox.innerHTML = '<div class="tasks"> <img class="check-logo" src="Logos/check.png" alt="check-logo"> <p>დავალება 1</p> <img class="trash-logo" src="Logos/trash.png" alt="trash"></img> </div>';
+
+    let trashIcons = document.querySelectorAll('.trash-logo');
+    trashIcons.forEach(icon => {
+        icon.addEventListener('click', removeTask);
+    });
+
+};
+
+function removeTask(event) {
+    const taskToRemove = event.target.closest('.tasks');
+
+    if (taskToRemove) {
+        taskToRemove.remove();
+    }
+};
 
